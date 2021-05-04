ef8cbc1ccd9c00018af9701b5c8b7ae9e54fe9db
EmmanueleLibraro <emmanuelelibraro95@gmail.com>
2021-05-04 09:45:02 +0200

    fine

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,16 @@
 const app = new Vue({
     el: '#app',
     data: {
-       email: []
+       email: [],
     },
     created(){
         for (let i = 0; i <= 10; i++){
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then(result => {
                 console.log(result.data);
-                this.email = result.data.response;
+                this.email.push(result.data.response);
+                
+                // return this.email.push(this.email);
             })
             .catch(error => {
                 console.log('Error', error);
